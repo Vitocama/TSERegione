4908cf5bfadf9c11ffd9f5727dc7483f50415bda
vitoCamarretta <vcamarretta@gmail.com>
2023-10-21 12:37:46 +0200

    Regione

---
+++ RegioneFIController.Custom.cs
@@ -0,0 +1,42 @@
+﻿
+using TeamSystem.AlyCE.Services.Controllers;
+using TeamSystem.AlyCE.Services.Common.Attributes;
+using TeamSystem.AlyCE.Shared;
+using TeamSystem.Alyante.Services.FI_GeneralData.DTO;
+using TeamSystem.Alyante.Services.FI_GeneralData.Managers;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using Microsoft.AspNetCore.Http;
+using TeamSystem.AlyCE.Services.Common.Results;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace TeamSystem.Alyante.Services.FI_GeneralData.Controllers
+{
+
+    public partial class RegioneFIController
+    {
+        public const string cittaOp = "citta";
+        [HttpPost(cittaOp)]
+        [SwaggerOperation(Summary = "citta", Description = "cittaoq")]
+        [SwaggerResponse(StatusCodes.Status200OK, "tutto ok", typeof(List<int>))]
+        public IActionResult GetRegione([FromBody, SwaggerRequestBody("attivato paraemntro", Required = true)] RegioneParametersDTO @params)
+        {
+            if (@params == null)
+            {
+                return BadRequest("regna il nulla");
+            }
+
+         List<int> result = Manager.GetRegione(@params);
+      
+
+            return StatusCode(StatusCodes.Status200OK, result);
+
+
+        }
+
+
+
+    }
+}
+
---
+++ RegioneFIService.cs
@@ -0,0 +1,60 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Linq;
+using TeamSystem.Alyante.CO_GeneralData;
+using TeamSystem.Alyante.FI_GeneralData;
+using TeamSystem.Alyante.FI_GeneralData.Service;
+using TeamSystem.Alyante.Services.FI_GeneralData.DTO;
+using TeamSystem.AlyCE.Biz;
+using TeamSystem.AlyCE.Common;
+using TeamSystem.AlyCE.Common.Authentication;
+using TeamSystem.AlyCE.Common.Session;
+
+namespace TeamSystem.Alyante.Biz.FI_GeneralData
+{
+    public class RegioneFIService : IRegioneFIService
+    {
+        private ISession _session;
+        private IBizFactory _factory;
+
+        public List<int> ActivaRegione(List<int> id, bool flag)
+        {
+           if((flag.Equals(false) && id.Any())){
+                
+            return null;
+            }
+           
+            IRegioneFIBiz regioneFIBiz = _factory.CreateBiz<IRegioneFIBiz>();
+
+            SimpleSearchRequestInfo searchNodes = new SimpleSearchRequestInfo(RegioneFI.Informations.EntitySetName);
+            
+            foreach(int index in id) 
+
+            searchNodes.AddSearch(RegioneFI.Informations.Regione,SearchComparer.NotEqual,index);
+
+
+           var e = regioneFIBiz.Search(searchNodes).Cast<RegioneFI>().ToList();
+           
+            List<int> ids = new List<int>();
+            ids.AddRange(e.Select(x => (int)x.Regione));
+
+
+
+
+
+            return ids;
+        }
+
+        public void SetSession(ISession session)
+        {
+            _session = session ?? throw new ArgumentNullException(nameof(session));
+            _factory = new BizFactory(_session);
+        }
+
+      
+    }
+}
